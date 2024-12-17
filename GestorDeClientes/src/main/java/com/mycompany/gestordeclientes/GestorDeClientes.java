6bc736aaa3d145c6c348d24fec5522794c2d2084
Leonel <109088095+DevLpb@users.noreply.github.com>
2024-12-17 12:00:13 -0300

    Gestor de clientes. Guardado inicial
    Inicio del proyecto de prueba de gestión de clientes, capacidad de guardar temporalmente lograda.

---
+++ GestorDeClientes/src/main/java/com/mycompany/gestordeclientes/GestorDeClientes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestordeclientes;
+
+/**
+ *
+ * @author pc
+ */
+public class GestorDeClientes {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.setVisible(true);
+    }
+}
