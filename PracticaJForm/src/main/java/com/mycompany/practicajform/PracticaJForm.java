aa6008771194530772050b46ea780c08fbeec3cd
Leonel <109088095+DevLpb@users.noreply.github.com>
2024-12-17 10:55:57 -0300

    Inicio de proyecto

---
+++ PracticaJForm/src/main/java/com/mycompany/practicajform/PracticaJForm.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicajform;
+
+/**
+ *
+ * @author pc
+ */
+public class PracticaJForm {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.setVisible(true);
+    }
+}
