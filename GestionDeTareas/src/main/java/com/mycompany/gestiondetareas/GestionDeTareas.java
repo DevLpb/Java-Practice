8be0ee81dca2fc958ddffac3a00777a0bee5b386
Leonel <leonelpiedrabuena04@gmail.com>
2024-12-19 15:41:32 -0300

    Diseño de formulario y funciones básicas
    Creado el formulario, la lista, el textarea, y los inputs.
    Trabajada la lógica de creación, edición y eliminación de forma local.
    Añadido un Event Listener para la selección de elementos de la lista
    Manejo de errores con try-catch y mejor uso de dialogs.
    
    Paso previo a la persistencia de datos

---
+++ GestionDeTareas/src/main/java/com/mycompany/gestiondetareas/GestionDeTareas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestiondetareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestionDeTareas {
+
+    public static void main(String[] args) {
+        Panel panel = new Panel();
+        panel.setVisible(true);
+    }
+}
