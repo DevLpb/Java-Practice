b25ab3bfd70b5d209991d0a5fdcbfc04498b1d71
Leonel-exo <leonelpiedrabuena04@gmail.com>
2025-01-13 09:33:14 -0300

    Creación de estructura inicial
    Creación de productos. Validación y manejo de errores.

---
+++ GestorDeProductos/GestorDeProductos/src/main/java/com/mycompany/gestordeproductos/GestorDeProductos.java
@@ -0,0 +1,15 @@
+package com.mycompany.gestordeproductos;
+
+import com.mycompany.gestordeproductos.vista.Frame;
+
+/**
+ *
+ * @author pc
+ */
+public class GestorDeProductos {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setVisible(true);
+    }
+}
